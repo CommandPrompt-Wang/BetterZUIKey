6204e3787a88da20a18f2ac513f0e8d63ba636ac
CommandPrompt <commandprompt@qq.com>
2026-09-17 03:21:01 +0800

    chore: bump version 1.5.2 (17) -> 1.6.0 (18)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -21,8 +21,8 @@ android {
         applicationId = "moe.lovefirefly.betterzuikey"
         minSdk = 27
         targetSdk = 36
-        versionCode = 17
-        versionName = "1.5.2"
+        versionCode = 18
+        versionName = "1.6.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
