d4625ca62100fdb58673c786cd1938bf7c1828d9
CommandPrompt <commandprompt@qq.com>
2026-09-22 21:24:26 +0800

    chore: bump version 1.7.0 (20) -> 1.7.1 (21)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -21,8 +21,8 @@ android {
         applicationId = "moe.lovefirefly.betterzuikey"
         minSdk = 27
         targetSdk = 36
-        versionCode = 20
-        versionName = "1.7.0"
+        versionCode = 21
+        versionName = "1.7.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
